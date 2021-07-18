b50d5ae12d4445f7c61cd02e01ba018e4c408132
jshields123 <jackshields@live.co.uk>
2021-07-18 21:10:29 +0100

    added in todo card and displayed hardcoded data

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="hello"></div>
+  <div class="todo-card">
+    <span>{{ todo.title }}</span>
+    <h3>Created at: {{ todo.time }} on {{ todo.date }}</h3>
+  </div>
 </template>
 
 <script>
@@ -8,6 +11,28 @@ export default {
   // props: {
   //   msg: String,
   // },
+  data() {
+    return {
+      todo: {
+        id: 123,
+        category: "work",
+        title: "Create a vue app",
+        description: "an app to track things I need to do",
+        location: "my laptop",
+        date: "18/07/2021",
+        time: "9:00",
+        creator: "Jack Shields",
+      },
+    };
+  },
 };
 </script>
 
+<style scoped>
+.todo-card {
+  padding: 20px;
+  width: 250px;
+  border: 2px solid blue;
+  margin-bottom: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TodoCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoCard from "@/components/TodoCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    TodoCard,
   },
 };
 </script>
